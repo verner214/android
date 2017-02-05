c052e9ac0d0253b0ef7ff443b7542368971ee5ac
verner214 <lars.waernulf+5@gmail.com>
2017-02-05 15:12:29 +0100

    repete added

--- miljo_elite.bat
+++ miljo_elite.bat
@@ -1,6 +1,7 @@
 rem se incheckad fil miljo_jobb i roten av android-repo
 rem java
-set JAVA_HOME="C:\Program Files\Android\Android Studio\jre"
+rem JAVA_HOME="C:\Program Files\Android\Android Studio\jre"
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_121
 set PATH=%PATH%;%JAVA_HOME%\bin
 
 rem gradle
@@ -8,7 +9,7 @@ set GRADLE_HOME=C:\android\gradle-3.2.1
 set PATH=%PATH%;%GRADLE_HOME%\bin
 
 rem android sdk, ANDROID_SDK_HOME används av bla avd manager för att skapa AVD's
-set ANDROID_HOME=c:\users\lars\appdata\local\android\sdk\
-set ANDROID_SDK_HOME=c:\users\lars\appdata\local\android\sdk\
+set ANDROID_HOME=c:\users\lars\appdata\local\android\sdk
+set ANDROID_SDK_HOME=c:\users\lars\appdata\local\android\sdk
 set PATH=%PATH%;%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools
 
