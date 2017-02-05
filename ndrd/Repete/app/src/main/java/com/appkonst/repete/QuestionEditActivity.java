20b0a11e142477b8bb8f25c36cf19d8f45071b3b
verner214 <lars.waernulf+5@gmail.com>
2017-02-05 15:28:20 +0100

    två aktivites till

---
+++ ndrd/Repete/app/src/main/java/com/appkonst/repete/QuestionEditActivity.java
@@ -0,0 +1,13 @@
+package com.appkonst.repete;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuestionEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_edit);
+    }
+}
