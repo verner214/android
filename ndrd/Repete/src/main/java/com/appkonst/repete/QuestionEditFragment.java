9ce0dc1fee6c3614a7f6cc22374d9703c7af2bbb
verner214 <lars.waernulf+5@gmail.com>
2017-03-05 16:11:24 +0100

    edit påbörjat

--- ndrd/Repete/src/main/java/com/appkonst/repete/QuestionEditFragment.java
+++ ndrd/Repete/src/main/java/com/appkonst/repete/QuestionEditFragment.java
@@ -1,28 +1,30 @@
 package com.appkonst.repete;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link QuestionEditFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link QuestionEditFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class QuestionEditFragment extends Fragment {
 
     private int mPos;
     private QAItem mQAItem;
     private final static String TAG = "QuestionEditFragment";
+    static final int REQUESTCODE_IMAGE_GET = Activity.RESULT_FIRST_USER;
+    ImageView mImgLarge;
+    EditText mTxeComments;
 
     public QuestionEditFragment() {
         // Required empty public constructor
@@ -54,10 +56,65 @@ public class QuestionEditFragment extends Fragment {
         TextView txtRubrik = (TextView) v.findViewById(R.id.txtRubrik);
         txtRubrik.setText((question ? "F " : "S ") + (mPos / 2 + 1) + "/" + QALab.Count() + " " + mQAItem.getArea1() + " - " + mQAItem.getArea2());
 
-        TextView txtComments = (TextView) v.findViewById(R.id.txtComments);
-        txtComments.setText(mQAItem.getComments());
+        mTxeComments = (EditText) v.findViewById(R.id.txeComments);
+        mTxeComments.setText(mQAItem.getComments());
+
+        mImgLarge = (ImageView) v.findViewById(R.id.imgLarge);
+
+        Button btnSelectPhoto = (Button)v.findViewById(R.id.btnSelectPhoto);
+        //mBtnSelectPhoto.setText("välj bild");
+        //btnSelectPhoto.setEnabled(true);
+        btnSelectPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //mBtnSave.setEnabled(true);
+                selectImage();
+            }
+        });
+
+        Button btnSaveComments = (Button)v.findViewById(R.id.btnSaveComments);
+        btnSaveComments.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveComments();
+            }
+        });
 
         return v;
+    }//onCreateView
+
+    private void saveComments() {
     }
+    //startar inbyggd Activity som väljer bild
+    private void selectImage() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(intent, REQUESTCODE_IMAGE_GET);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        try {
+//visa vald bild i view, spara i molnet och
+            if (requestCode == REQUESTCODE_IMAGE_GET && resultCode == Activity.RESULT_OK) {
+                Uri selectedImageURI = data.getData();
+                boolean b2 = selectedImageURI == null;
+
+                Bitmap bmpLarge = ImageLibrary.Uri2Bmp(getActivity(), selectedImageURI, 640, 360, true);
+                boolean b = bmpLarge == null;
+
+                Toast.makeText(getActivity(), "onActivityResult=" + b2 + "," + b, Toast.LENGTH_SHORT).show();
+                mImgLarge.setImageBitmap(bmpLarge);
+//spara bild i molnet
+
+            }
+        } catch (Exception e) {
+            Toast.makeText(getActivity(), "exception=" + e.toString(), Toast.LENGTH_LONG).show();
+            Log.e(TAG, "Error downloading image", e);
+        }
+
+    }//onActivityResult
 
 }
